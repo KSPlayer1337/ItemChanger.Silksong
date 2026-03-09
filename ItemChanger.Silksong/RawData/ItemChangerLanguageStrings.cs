eb1c2f1e34b401fb421defa907c2ed5fbe7e0407
homothetyhk <homothetyhk@gmail.com>
2026-03-08 21:26:13 -0700

    Add CustomSkillModule and CustomSkillItem. Reimplement custom skills using these, and reorganize base item list.

--- ItemChanger.Silksong/RawData/ItemChangerLanguageStrings.cs
+++ ItemChanger.Silksong/RawData/ItemChangerLanguageStrings.cs
@@ -15,6 +15,24 @@ internal static class ItemChangerLanguageStrings
     public static LanguageString FMT_MATERIUM_ENTRY_NAME => LanguageString.FromItemChanger(nameof(FMT_MATERIUM_ENTRY_NAME));
     public static LanguageString FMT_JOURNAL_ENTRY_NAME => LanguageString.FromItemChanger(nameof(FMT_JOURNAL_ENTRY_NAME));
 
+    public static LanguageString INV_NAME_SKILL_HARPOON_LEFT => LanguageString.FromItemChanger(nameof(INV_NAME_SKILL_HARPOON_LEFT));
+    public static LanguageString INV_NAME_SKILL_HARPOON_RIGHT => LanguageString.FromItemChanger(nameof(INV_NAME_SKILL_HARPOON_RIGHT));
+    public static LanguageString INV_DESC_SKILL_HARPOON_LEFT => LanguageString.FromItemChanger(nameof(INV_DESC_SKILL_HARPOON_LEFT));
+    public static LanguageString INV_DESC_SKILL_HARPOON_RIGHT => LanguageString.FromItemChanger(nameof(INV_DESC_SKILL_HARPOON_RIGHT));
+    public static LanguageString INV_NAME_WALLJUMP_LEFT => LanguageString.FromItemChanger(nameof(INV_NAME_WALLJUMP_LEFT));
+    public static LanguageString INV_NAME_WALLJUMP_RIGHT => LanguageString.FromItemChanger(nameof(INV_NAME_WALLJUMP_RIGHT));
+    public static LanguageString INV_DESC_WALLJUMP_LEFT => LanguageString.FromItemChanger(nameof(INV_DESC_WALLJUMP_LEFT));
+    public static LanguageString INV_DESC_WALLJUMP_RIGHT => LanguageString.FromItemChanger(nameof(INV_DESC_WALLJUMP_RIGHT));
+    public static LanguageString INV_NAME_SKILL_SPRINT_LEFT => LanguageString.FromItemChanger(nameof(INV_NAME_SKILL_SPRINT_LEFT));
+    public static LanguageString INV_NAME_SKILL_SPRINT_RIGHT => LanguageString.FromItemChanger(nameof(INV_NAME_SKILL_SPRINT_RIGHT));
+    public static LanguageString INV_DESC_SKILL_SPRINT_LEFT => LanguageString.FromItemChanger(nameof(INV_DESC_SKILL_SPRINT_LEFT));
+    public static LanguageString INV_DESC_SKILL_SPRINT_RIGHT => LanguageString.FromItemChanger(nameof(INV_DESC_SKILL_SPRINT_RIGHT));
+    public static LanguageString INV_NAME_LEFTSLASH => LanguageString.FromItemChanger(nameof(INV_NAME_LEFTSLASH));
+    public static LanguageString INV_NAME_RIGHTSLASH => LanguageString.FromItemChanger(nameof(INV_NAME_RIGHTSLASH));
+    public static LanguageString INV_NAME_UPSLASH => LanguageString.FromItemChanger(nameof(INV_NAME_UPSLASH));
+    public static LanguageString INV_NAME_DOWNSLASH => LanguageString.FromItemChanger(nameof(INV_NAME_DOWNSLASH));
+    public static LanguageString INV_DESC_ANYSLASH => LanguageString.FromItemChanger(nameof(INV_DESC_ANYSLASH));
+
     public static CompositeString CreatePayRosariesString(IValueProvider<int> rosaryCount)
     {
         return CompositeString.Create(FMT_PAY_ROSARIES, new Dictionary<string, IValueProvider<object>>()
