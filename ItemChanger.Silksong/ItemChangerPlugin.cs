3da9818f149b8fc7ca02b378b87140d0afa3d98f
homothetyhk <45406381+homothetyhk@users.noreply.github.com>
2026-03-15 09:53:46 -0700

    Revert dependency change

--- ItemChanger.Silksong/ItemChangerPlugin.cs
+++ ItemChanger.Silksong/ItemChangerPlugin.cs
@@ -12,7 +12,7 @@ namespace ItemChanger.Silksong
     [BepInDependency("org.silksong-modding.prepatcher")]
     [BepInDependency("org.silksong-modding.i18n")]
     [BepInDependency("org.silksong-modding.datamanager")]
-    [BepInDependency("io.github.benchwarp")]
+    [BepInDependency("io.github.homothetyhk.benchwarp")]
     [BepInAutoPlugin(id: "io.github.silksong.itemchanger")]
     public partial class ItemChangerPlugin : BaseUnityPlugin, IRawSaveDataMod
     {
